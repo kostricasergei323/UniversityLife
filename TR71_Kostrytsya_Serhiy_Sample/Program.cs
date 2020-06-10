94b6f236c6abe9be9eac8eb620719e444537c449
Sergei <kostricasergei323@gmail.com>
2020-06-10 19:55:19 +0300

    Added solution for future program realization

---
+++ TR71_Kostrytsya_Serhiy_Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR71_Kostrytsya_Serhiy_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
