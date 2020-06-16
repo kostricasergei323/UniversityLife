aecbf650e224bc73472af8a7b0958fa9dd7c99dd
Kostrica Sergei <kostricasergei323@gmail.com>
2020-06-16 13:48:49 +0300

    Added Builder pattern for User.cs file

---
+++ DAL/Entities/SystemRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public enum SystemRole
+    {
+        Administrator = 0,
+        Moderator = 1,
+        Student = 2
+    }
+}
---
+++ DAL/Entities/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public SystemRole UserRole { get; set; }
+    }
+
+    public abstract class UserBuilder
+    {
+        public abstract void FillUserIdAndName(int userId, string name);
+        public abstract void FillUserCredentials(string login, string password);
+        public abstract void FillUserRole(SystemRole userRole);
+        public abstract User GetUser();
+    }
+
+    public class ConcreteUserBuilder: UserBuilder
+    {
+        public User user = new User();
+
+        public override void FillUserIdAndName(int userId, string name)
+        {
+            user.UserId = userId;
+            user.Name = name;
+        }
+
+        public override void FillUserCredentials(string login, string password)
+        {
+            user.Login = login;
+            user.Password = password;
+        }
+
+        public override void FillUserRole(SystemRole userRole)
+        {
+            user.UserRole = userRole;
+        }
+
+        public override User GetUser()
+        {
+            return user;
+        }
+    }
+}
